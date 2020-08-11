e308286e21e95afb0bb4c8a42bc7c6cf74372069
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-11 13:38:22 -0700

    update post title

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -45,4 +45,43 @@ router.get('/', (req, res) => {
       });
   });
 
+  // router.post('/', (req, res) => {
+  //   // expects {title: 'Taskmaster goes public!', post_url: 'https://taskmaster.com/press', user_id: 1}
+  //   Post.create({
+  //     title: req.body.title,
+  //     post_url: req.body.post_url,
+  //     user_id: req.body.user_id
+  //   })
+  //     .then(dbPostData => res.json(dbPostData))
+  //     .catch(err => {
+  //       console.log(err);
+  //       res.status(500).json(err);
+  //     });
+  // });
+
+
+  router.put('/:id', (req, res) => {
+    Post.update(
+      {
+        title: req.body.title
+      },
+      {
+        where: {
+          id: req.params.id
+        }
+      }
+    )
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
+
   module.exports = router;
