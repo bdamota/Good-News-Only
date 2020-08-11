96bd08296091e61e6ecc34cf79dd05b35690f630
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-11 11:02:04 -0700

    model associations added

--- models/index.js
+++ models/index.js
@@ -1,5 +1,14 @@
 const User = require('./User');
 const Post = require("./Post");
 
+// create associations
+User.hasMany(Post, {
+    foreignKey: 'id'
+  });
+
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+  });
+
 module.exports = { User };
 module.exports = { User, Post };
