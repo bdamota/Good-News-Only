abf4e45b0d45a2306b842dc0088ce350b33d3acc
LYNN DAMOTA <bdamota0506@gmail.com>
2020-08-21 01:06:20 -0700

    helper functions added

--- server.js
+++ server.js
@@ -3,12 +3,13 @@ const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const path = require('path');
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
-const session = require('express-session');
-
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+const session = require('express-session');
+const helpers = require('./utils/helpers');
+const hbs = exphbs.create({ helpers });
 
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -21,6 +22,7 @@ const sess = {
     db: sequelize
   })
 };
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
